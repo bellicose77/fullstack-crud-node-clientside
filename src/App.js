59d4b708b3200a828f0b0967f81ac03eae32c435
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-20 14:57:25 +0600

    link done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Route, Router, Routes } from "react-router-dom";
+import AddUser from "./componets/AddUser/AddUser";
 import Header from "./componets/Header/Header";
+import Home from "./componets/Home/Home";
+import UpdateUser from "./componets/UpdateUser/UpdateUser";
 
 
 
@@ -7,8 +10,11 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
-          <Route path="/" element={<Header/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path ='/adduser' element={<AddUser/>}/>
+          <Route path ='/update user' element={<UpdateUser/>}/>
         </Routes>
       </Router>
     </div>
