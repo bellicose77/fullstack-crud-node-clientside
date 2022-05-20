e4327b74ed221f75b8809f78dbd0fce8cc3ddc29
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-20 14:54:39 +0600

    home

---
+++ src/componets/UpdateUser/UpdateUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateUser = () => {
+    return (
+        <div>
+            <h2>This is update user</h2>
+        </div>
+    );
+};
+
+export default UpdateUser;
