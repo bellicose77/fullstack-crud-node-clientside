5097ae3dc9660c2f314c591da347df4f4815a83d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-22 21:42:15 +0600

    got data

--- src/componets/AddUser/AddUser.js
+++ src/componets/AddUser/AddUser.js
@@ -3,8 +3,12 @@ import React, { useRef, useState } from 'react';
 const AddUser = () => {
     const [user,setUser] = useState([]);
     const nameref = useRef('');
-    const emailref = useRef('')
+    const emailref = useRef('');
     const handleSubmit = (e) =>{
+        const name = nameref.current.value;
+        const email = emailref.current.value;
+        console.log(name);
+        console.log(email);
        e.preventDefault();
        console.log("hitting the button");
     }
@@ -12,8 +16,8 @@ const AddUser = () => {
         <div>
             <h2>Add user here</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="enter name"/>
-                <input type="email" name ="email" placeholder="email"/>
+                <input type="text" name="name" ref={nameref} placeholder="enter name"/>
+                <input type="email" name ="email" ref={emailref} placeholder="email"/>
                 <input type="submit"/>
             </form>
         </div>
