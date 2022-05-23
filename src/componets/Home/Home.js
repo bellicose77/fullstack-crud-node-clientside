92b7adc69f05fe4422a6e608d828e4b66318f367
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-24 01:13:22 +0600

    get api done

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-
+   const [user ,setUser] = useState([]);
     useEffect(()=>{
         fetch('http://localhost:5000/user')
+        .then(res =>res.json())
+        .then(data=>setUser(data));
     },[])
     return (
         <div>
-           
+           <h>Total user in : {user.length}</h>
         </div>
     );
 };
