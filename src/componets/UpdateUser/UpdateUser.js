624ec63f74db3268c07777f2ff26a1d37c3cf1ea
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-24 23:37:43 +0600

    fetch func

--- src/componets/UpdateUser/UpdateUser.js
+++ src/componets/UpdateUser/UpdateUser.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
     const {id} = useParams();
+    useEffect(()=>{
+        const url = `http://localhost:5000/user/${id}`
+        fetch(url)
+        .then(res=>res.json())
+    },[])
     return (
         <div>
             <h2>This is update user: {id}</h2>
