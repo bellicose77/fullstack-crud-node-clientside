b7b5d2ca88f70df105178231014b24a509a9e6fc
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-24 21:59:41 +0600

    done

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -13,6 +13,16 @@ const Home = () => {
        fetch(url,{
            method:'DELETE'
        })
+       .then(res=>res.json())
+       .then(data=>{
+           if(data.deletedCount)
+           {
+               alert("deleted successfully");
+               const remaining=users.filter(user=> user._id!=id);
+               setUsers(remaining);
+           }
+       }
+        )
     }
     return (
         <div>
