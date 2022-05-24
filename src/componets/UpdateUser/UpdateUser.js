dd0888afd62fe9e8f4cc3eaeedd26a09cf81416d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-05-24 23:28:45 +0600

    update

--- src/componets/UpdateUser/UpdateUser.js
+++ src/componets/UpdateUser/UpdateUser.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+    const {id} = useParams();
     return (
         <div>
-            <h2>This is update user</h2>
+            <h2>This is update user: {id}</h2>
         </div>
     );
 };
